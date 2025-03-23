111eaadd05d477d08df44462c722e3591383c3e9
Shy <me@shyy.io>
2025-03-22 22:00:56 -0400

    update URL cleaning

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="textcleaning",
-    version="4.0",
+    version="4.1",
     description="Text cleaning",
     packages=["textcleaning"],
     install_requires=[
